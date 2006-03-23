fd9bbe49d53a2a9f52bc3f937b51108619edc502
nobody <nobody@8c1e88eb-08f3-148f-2d68-3308c1931f23>
2006-03-23 18:45:55 +0000

    This commit was manufactured by cvs2svn to create tag 'xwork_1-1-3'.

---
+++ tags/xwork_1-1-3/src/java/com/opensymphony/xwork/interceptor/ModelDrivenInterceptor.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2002-2006 by OpenSymphony
+ * All rights reserved.
+ */
+package com.opensymphony.xwork.interceptor;
+
+import com.opensymphony.xwork.ActionInvocation;
+import com.opensymphony.xwork.ModelDriven;
+import com.opensymphony.xwork.util.OgnlValueStack;
+
+
+/**
+ * <!-- START SNIPPET: description -->
+ *
+ * Watches for {@link ModelDriven} actions and adds the action's model on to the value stack.
+ *
+ * <p/> <b>Note:</b>  The ModelDrivenInterceptor must come before the both {@link StaticParametersInterceptor} and
+ * {@link ParametersInterceptor} if you want the parameters to be applied to the model.
+ *
+ * <!-- END SNIPPET: description -->
+ *
+ * <p/> <u>Interceptor parameters:</u>
+ *
+ * <!-- START SNIPPET: parameters -->
+ *
+ * <ul>
+ *
+ * <li>None</li>
+ *
+ * </ul>
+ *
+ * <!-- END SNIPPET: parameters -->
+ *
+ * <p/> <u>Extending the interceptor:</u>
+ *
+ * <p/>
+ *
+ * <!-- START SNIPPET: extending -->
+ *
+ * There are no known extension points to this interceptor.
+ *
+ * <!-- END SNIPPET: extending -->
+ *
+ * <p/> <u>Example code:</u>
+ *
+ * <pre>
+ * <!-- START SNIPPET: example -->
+ * &lt;action name="someAction" class="com.examples.SomeAction"&gt;
+ *     &lt;interceptor-ref name="model-driven"/&gt;
+ *     &lt;interceptor-ref name="basicStack"/&gt;
+ *     &lt;result name="success"&gt;good_result.ftl&lt;/result&gt;
+ * &lt;/action&gt;
+ * <!-- END SNIPPET: example -->
+ * </pre>
+ */
+public class ModelDrivenInterceptor extends AroundInterceptor {
+
+    protected void after(ActionInvocation dispatcher, String result) throws Exception {
+    }
+
+    protected void before(ActionInvocation invocation) throws Exception {
+        Object action = invocation.getAction();
+
+        if (action instanceof ModelDriven) {
+            ModelDriven modelDriven = (ModelDriven) action;
+            OgnlValueStack stack = invocation.getStack();
+            stack.push(modelDriven.getModel());
+        }
+    }
+}
---
+++ tags/xwork_1-1-3/src/java/com/opensymphony/xwork/validator/validators/EmailValidator.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2002-2006 by OpenSymphony
+ * All rights reserved.
+ */
+package com.opensymphony.xwork.validator.validators;
+
+
+/**
+ * <!-- START SNIPPET: javadoc -->
+ * EmailValidator checks that a given String field, if not empty,
+ * is a valid email address.
+ * <p/>
+ * <p/>
+ * The regular expression used to validate that the string is an email address
+ * is:
+ * </p>
+ * <pre>
+ * "\\b(^(\\S+@).+((\\.com)|(\\.net)|(\\.org)|(\\.info)|(\\.edu)|(\\.mil)|(\\.gov)|(\\.biz)|(\\.ws)|(\\.us)|(\\.tv)|(\\.cc)|(\\..{2,2}))$)\\b"
+ * </pre>
+ * <!-- END SNIPPET: javadoc -->
+ * 
+ * 
+ * <!-- START SNIPPET: parameters -->
+ * <ul>
+ * 		<li>fieldName - The field name this validator is validating. Required if using Plain-Validator Syntax otherwise not required</li>
+ * </ul>
+ * <!-- END SNIPPET: parameters -->
+ * 
+ * 
+ * <pre>
+ * <!-- START SNIPPET: example -->
+ *     &lt;!-- Plain Validator Syntax --&gt;
+ *     &lt;validators&gt;
+ *         &lt;validator type="email"&gt;
+ *             &lt;param name="fieldName"&gt;myEmail&lt;/param&gt;
+ *             &lt;message&gt;Must provide a valid email&lt;/message&gt;
+ *         &lt;/validator&gt;
+ *     &lt;/validators&gt;
+ *     
+ *     &lt;!-- Field Validator Syntax --&gt;
+ *     &lt;field name="myEmail"&gt;
+ *        &lt;field-validator type="email"&gt;
+ *           &lt;message&gt;Must provide a valid email&lt;/message&gt;
+ *        &lt;/field-validator&gt;
+ *     &lt;/field&gt;
+ * <!-- END SNIPPET: example -->
+ * </pre>
+ *
+ * @author jhouse
+ */
+public class EmailValidator extends StringRegexValidator {
+
+    public static final String emailAddressPattern =
+            "\\b(^(\\S+@).+((\\.com)|(\\.net)|(\\.org)|(\\.info)|(\\.edu)|(\\.mil)|(\\.gov)|(\\.biz)|(\\.ws)|(\\.us)|(\\.tv)|(\\.cc)|(\\.aero)|(\\.arpa)|(\\.coop)|(\\.int)|(\\.jobs)|(\\.museum)|(\\.name)|(\\.pro)|(\\.travel)|(\\.nato)|(\\..{2,2}))$)\\b";
+
+
+    public EmailValidator() {
+        setRegex(emailAddressPattern);
+        setCaseSensitive(false);
+    }
+
+}
+
+
