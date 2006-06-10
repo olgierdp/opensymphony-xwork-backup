d67ccf7aee88e2f9c81e91a152ef0c71be2fe5f1
nobody <nobody@8c1e88eb-08f3-148f-2d68-3308c1931f23>
2006-06-10 05:53:37 +0000

    This commit was manufactured by cvs2svn to create branch 'xwork_1-2'.

---
+++ branches/xwork_1-2/src/java/com/opensymphony/xwork/interceptor/DefaultWorkflowInterceptor.java
@@ -0,0 +1,131 @@
+/*
+ * Copyright (c) 2002-2006 by OpenSymphony
+ * All rights reserved.
+ */
+package com.opensymphony.xwork.interceptor;
+
+import com.opensymphony.xwork.Action;
+import com.opensymphony.xwork.ActionInvocation;
+import com.opensymphony.xwork.Validateable;
+import com.opensymphony.xwork.ValidationAware;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * <!-- START SNIPPET: description -->
+ *
+ * An interceptor that does some basic validation workflow before allowing the interceptor chain to continue.
+ *
+ * <p/>This interceptor does nothing if the name of the method being invoked is specified in the <b>excludeMethods</b>
+ * parameter. <b>excludeMethods</b> accepts a comma-delimited list of method names. For example, requests to
+ * <b>foo!input.action</b> and <b>foo!back.action</b> will be skipped by this interceptor if you set the
+ * <b>excludeMethods</b> parameter to "input, back".
+ *
+ * <p/>The order of execution in the workflow is:
+ *
+ * <ol>
+ *
+ * <li>If the action being executed implements {@link Validateable}, the action's {@link Validateable#validate()
+ * validate} method is called.</li>
+ *
+ * <li>Next, if the action implements {@link ValidationAware}, the action's {@link ValidationAware#hasErrors()
+ * hasErrors} method is called. If this method returns true, this interceptor stops the chain from continuing and
+ * immediately returns {@link Action#INPUT}</li>
+ *
+ * </ol>
+ *
+ * <p/> Note: if the action doesn't implement either interface, this interceptor effectively does nothing. This
+ * interceptor is often used with the <b>validation</b> interceptor. However, it does not have to be, especially if you
+ * wish to write all your validation rules by hand in the validate() method rather than in XML files.
+ *
+ * <p/>
+ *
+ * <b>NOTE:</b> As this method extends off MethodFilterInterceptor, it is capable of
+ * deciding if it is applicable only to selective methods in the action class. See
+ * <code>MethodFilterInterceptor</code> for more info.
+ *
+ * <!-- END SNIPPET: description -->
+ *
+ * <p/> <u>Interceptor parameters:</u>
+ *
+ * <!-- START SNIPPET: parameters -->
+ *
+ * <ul>
+ *
+ * <li>None</li>
+ *
+ * </ul>
+ *
+ * <!-- END SNIPPET: parameters -->
+ *
+ * <p/> <u>Extending the interceptor:</u>
+ *
+ * <p/>
+ *
+ * <!-- START SNIPPET: extending -->
+ *
+ * There are no known extension points for this interceptor.
+ *
+ * <!-- END SNIPPET: extending -->
+ *
+ * <p/> <u>Example code:</u>
+ *
+ * <pre>
+ * <!-- START SNIPPET: example -->
+ * 
+ * &lt;action name="someAction" class="com.examples.SomeAction"&gt;
+ *     &lt;interceptor-ref name="params"/&gt;
+ *     &lt;interceptor-ref name="validation"/&gt;
+ *     &lt;interceptor-ref name="workflow"/&gt;
+ *     &lt;result name="success"&gt;good_result.ftl&lt;/result&gt;
+ * &lt;/action&gt;
+ * 
+ * &lt;-- In this case myMethod of the action class will not pass through 
+ *        the workflow process --&gt;
+ * &lt;action name="someAction" class="com.examples.SomeAction"&gt;
+ *     &lt;interceptor-ref name="params"/&gt;
+ *     &lt;interceptor-ref name="validation"/&gt;
+ *     &lt;interceptor-ref name="workflow"&gt;
+ *         &lt;param name="excludeMethods"&gt;myMethod&lt;/param&gt;
+ *     &lt;/interceptor-ref name="workflow"&gt;
+ *     &lt;result name="success"&gt;good_result.ftl&lt;/result&gt;
+ * &lt;/action&gt;
+ * 
+ * <!-- END SNIPPET: example -->
+ * </pre>
+ *
+ * @author Jason Carreira
+ * @author Rainer Hermanns
+ * @author <a href='mailto:the_mindstorm[at]evolva[dot]ro'>Alexandru Popescu</a>
+ * 
+ * @version $Date$ $Id$
+ */
+public class DefaultWorkflowInterceptor extends MethodFilterInterceptor {
+	
+	private static final Log _log = LogFactory.getLog(DefaultWorkflowInterceptor.class);
+	
+    protected String doIntercept(ActionInvocation invocation) throws Exception {
+        Object action = invocation.getAction();
+
+        if (action instanceof Validateable) {
+            Validateable validateable = (Validateable) action;
+            if (_log.isDebugEnabled()) {
+            	_log.debug("Invoking validate() on action "+validateable);
+            }
+            validateable.validate();
+        }
+
+        if (action instanceof ValidationAware) {
+            ValidationAware validationAwareAction = (ValidationAware) action;
+
+            if (validationAwareAction.hasErrors()) {
+            	if (_log.isDebugEnabled()) {
+            		_log.debug("Errors on action "+validationAwareAction+", returning result name 'input'");
+            	}
+                return Action.INPUT;
+            }
+        }
+
+        return invocation.invoke();
+    }
+}
---
+++ branches/xwork_1-2/src/test/com/opensymphony/xwork/interceptor/PrepareInterceptorTest.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2002-2006 by OpenSymphony
+ * All rights reserved.
+*/
+package com.opensymphony.xwork.interceptor;
+
+import com.mockobjects.dynamic.Mock;
+import com.opensymphony.xwork.mock.MockActionInvocation;
+import com.opensymphony.xwork.Preparable;
+import com.opensymphony.xwork.Action;
+import com.opensymphony.xwork.SimpleFooAction;
+import junit.framework.TestCase;
+
+/**
+ * Unit test for PrepareInterceptor.
+ *
+ * @author Claus Ibsen
+ */
+public class PrepareInterceptorTest extends TestCase {
+
+    private Mock mock;
+    private PrepareInterceptor interceptor;
+
+    public void testPrepareCalled() throws Exception {
+        MockActionInvocation mai = new MockActionInvocation();
+        mai.setAction(mock.proxy());
+        mock.expect("prepare");
+
+        interceptor.before(mai);
+        interceptor.after(mai, Action.SUCCESS); // to have higher code coverage
+    }
+
+    public void testNoPrepareCalled() throws Exception {
+        MockActionInvocation mai = new MockActionInvocation();
+        mai.setAction(new SimpleFooAction());
+
+        interceptor.before(mai);
+        interceptor.after(mai, Action.SUCCESS); // to have higher code coverage
+    }
+
+    protected void setUp() throws Exception {
+        mock = new Mock(Preparable.class);
+        interceptor = new PrepareInterceptor();
+    }
+
+    protected void tearDown() throws Exception {
+        mock.verify();
+    }
+
+}
